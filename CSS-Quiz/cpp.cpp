090c646941af8e96c9f04b56a5ba5fe015351b03
UsmanMehmood7 <109785023+UsmanMehmood7@users.noreply.github.com>
2022-08-28 16:04:03 +0500

    Add files via upload

---
+++ CSS-Quiz/cpp.cpp
@@ -0,0 +1,35 @@
+#include  <iostream>
+using namespace std;
+int main ()
+{
+
+    for(int i=0;i<5;i++){
+        cout<<i<<" hello world\n";
+
+
+    }
+
+int arr[]={1,2,3};
+// for(int i=0;i<3;i++){
+//     if(arr[i]==2)
+//     cout<<arr[i];
+    
+
+// }
+int count=2;
+cout<<arr[count];
+int a =3;
+int b=4;
+int swap;
+swap=a; 
+a=b;    
+b=swap; 
+
+
+
+
+
+
+
+    return 0;
+}
